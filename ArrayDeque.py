a11ff6e794711d74efdf4fb5e32b7c6907c29f3d
Joff <mail@jfsalzmann.com>
2023-03-16 16:58:18 +0100

    submission

--- ArrayDeque.py
+++ ArrayDeque.py
@@ -2,14 +2,75 @@
 """
 Created on Sun Mar  5 18:34:01 2023
 
-@author: Hannah
+@author: Johann-Friedrich Salzmann, Finn Krüger
 """
 
 class Empty(Exception): pass
 
 
-class ArrayDequeMaxlen(): pass
+class ArrayDequeMaxlen():
+    
+    def __init__(self,max):
+        # This implementation is done with python lists, so no need to have a front pointer
+        # and the order of the list will consistently indicate the order of the queue
+        self._data = []
+        self.__max = max
         
+    def __len__(self): # length of AQM should be length of the list
+        return len(self._data)
+    
+    def __getitem__(self, index): # direct AQM element access
+        return self._data[index]
+    
+    def __repr__(self): # string representation of AQM
+        return str(self._data)
+    
+    @property
+    def _front(self):
+        # This implementation is done with python lists, so no need to have a front pointer
+        # and the order of the list will consistently indicate the order of the queue
+        # -> This is only added for interface compatibility reasons
+        return 1
+        
+    def add_first(self,e): # add element in front of the queue and delete at the end if max length reached
+        if(self.__len__() == self.__max):
+            self.delete_last()
+        self._data.insert(0,e)
+        
+    def add_last(self,e): # add element at the end of the queue and delete in front if max length reached
+        if(self.__len__() == self.__max):
+            self.delete_first()
+        self._data.append(e)
+        
+    def delete_first(self): # delete element in front
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data.pop(0)
+        
+    def delete_last(self): # delete element at the end
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data.pop()
+        
+    def remove(self,e): # remove all elements of a given value from the queue
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        self._data = [x for x in self._data if x != e]
+        
+    def first(self): # return first element
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data[0]
+        
+    def last(self): # return last element
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data[-1]
+        
+    def is_empty(self): # return whether queue is empty
+        return len(self._data) == 0
+        
+    
                 
         
         
--- main.py
+++ main.py
@@ -2,11 +2,21 @@
 """
 Created on Sun Mar  5 18:34:20 2023
 
-@author: Hannah
+@author: Johann-Friedrich Salzman, Finn Krüger
 """
 
 from ArrayDeque import ArrayDequeMaxlen
 
+# PLEASE NOTE
+# We noticed that the commands given in the main.py initially
+# did not at all match the interface definition of the AQM class
+# as provided at Github. Additionally, some methods used here were
+# not defined in the task on Github. We handled the situation as
+# follows: We followed the names and basic instructions on Github,
+# and added the missing methods according to the main.py.
+# As stated in the class, we relied on not having to implement
+# the AQM with Arrays, so we have a dynamic length list underlying
+# with no need to have length adjustment, or use a front pointer.
      
         
 AQM = ArrayDequeMaxlen(20)
@@ -14,18 +24,18 @@ AQM = ArrayDequeMaxlen(20)
 
 print('Adding last')
 for i in range(100):
-    AQM.append(i)
-    print (i, AQM._data)
+    AQM.add_last(i)
+    print (i, AQM)
     
-print('\nDelete 80', AQM.remove(80), AQM._data, AQM._front)
+print('\nDelete 80', AQM.remove(80), AQM, AQM._front)
     
 
 
     
 print ('\nAdding first')
 for i in range(20, 10, -1):
-    AQM.appendleft(i)
-    print (i, AQM._data)
+    AQM.add_first(i)
+    print (i, AQM)
     
     
 print(AQM._front)
@@ -34,4 +44,4 @@ print(AQM._front)
     
 print('\nPerforming the removals')
 while not AQM.is_empty():
-    print ('Remove first', AQM[0], AQM.popleft(), 'Remove last', AQM[-1],  AQM.pop())
+    print ('Remove first', AQM.first(), AQM.delete_first(), 'Remove last', AQM.last(),  AQM.delete_last())
